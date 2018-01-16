ff9f3ab1d6652fda8b0f94ab880ddfb6ac5e2eb5
Shion <f.shion1118@gmail.com>
2018-01-16 22:00:51 +0900

    add: Hash(sha256)

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,9 @@
+import com.google.gson.Gson;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by shion on 2018/01/15.
  */
@@ -10,5 +16,26 @@ public class Block {
     public Block(String previousHash, long timeStamp) {
         this.previousHash = previousHash;
         this.timeStamp = timeStamp;
+    }
+
+    public String toString() {
+        return new Gson().toJson(this).toString();
+    }
+
+    public static String hash(Block block) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(block.toString().getBytes(StandardCharsets.UTF_8));
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
