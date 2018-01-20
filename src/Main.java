73a834660f3a13e293bca9cd9188fe0628a65d57
Shion <f.shion1118@gmail.com>
2018-01-20 13:32:18 +0900

    add: Basic progress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
 
+    public static Blockchain blockchain;
+
     public static void main(String[] args) {
+        blockchain = new Blockchain();
+        mine();
+        System.out.println(blockchain.getChain());
+        System.out.println(blockchain.validChain());
+    }
+
+    public static void mine() {
+        Block laskBlock = blockchain.getLastBlock();
+        int lastProof = laskBlock.getProof();
+
+        blockchain.newTransaction("0", "1", (long)1.0);
+        int proof = blockchain.calculatePoW(lastProof);
+        blockchain.newBlock(proof, laskBlock.hash());
     }
 }
